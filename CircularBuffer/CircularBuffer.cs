05500ca4d1b4795d9b2640362fce4c3b24f7dd59
John Lundgren <john.paul.lundgren@gmail.com>
2018-12-03 08:14:56 +0100

    First commit

---
+++ CircularBuffer/CircularBuffer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace CircularBuffer
+{
+
+    /// <summary>
+    /// First in First out FIFO algorithm.
+    /// </summary>
+    public class CircularBuffer
+    {
+        private readonly int[] _content;
+
+        //Index for the data that is going to be retrieved.
+        private int _tail = 0;
+        //Index for the data that is going to be added.
+        private int _head = 0;
+        //Count of existing elements in the data.
+        private int _count = 0;
+
+        public CircularBuffer(int maxSize)
+        {
+            _content = new int[maxSize];
+        }
+
+        private void OverWriteData()
+        {
+
+        }
+
+        public int AddValue(int value)
+        {
+            _head = _head % this._content.Length;
+            _content[_head] = value;
+            if(_count == _content.Length)
+            {
+                _tail++;
+            }
+            else
+            {
+                _count++;
+            }
+            _head++;
+            return _head;
+        }
+
+        public int RetrieveValue()
+        {
+            if(_count == 0)
+            {
+                throw new Exception("No value available in buffer");
+            }
+
+            int value = _content[_tail];
+            _tail++;
+            _count--;
+            return value;
+        }
+
+        public int[] GetBufferContent()
+        {
+            int[] temp = new int[_content.Length];
+            Array.Copy(_content, temp, this._content.Length);
+            return temp;
+        }
+    }
+}
---
+++ CircularBuffer/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace CircularBuffer
+{
+    class MainClass
+    {
+
+        public static string AskForString(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+
+        public static int AskForInt(string prompt)
+        {
+            string answer = AskForString(prompt);
+            if(int.TryParse(answer, out int value))
+            {
+                return value;
+            }
+            else
+            {
+                throw new FormatException("Value not an integer!!!");
+            }
+        }
+
+        public static void AddValue(CircularBuffer buffer)
+        {
+            try
+            {
+                int value = AskForInt("Write value to add to the buffer: ");
+                buffer.AddValue(value);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void RetrieveValue(CircularBuffer buffer)
+        {
+            try
+            {
+                int value = buffer.RetrieveValue();
+                Console.WriteLine("Value retrieved: {0}", value);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("No value available in buffer.");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+
+            string menuOption = string.Empty;
+            CircularBuffer buffer = new CircularBuffer(3);
+
+            do
+            {
+                menuOption = AskForString("0.Quit\n1.Add value\n2.Retrieve value\n");
+                switch (menuOption)
+                {
+                    case "0":
+                        break;
+                    case "1":
+                        AddValue(buffer);
+                        break;
+                    case "2":
+                        RetrieveValue(buffer);
+                        break;
+                    default:
+                        Console.WriteLine("Not an available option");
+                        break;
+                }
+            } while (menuOption != "0");
+
+            Console.WriteLine("Program ends!");
+        }
+    }
+}
